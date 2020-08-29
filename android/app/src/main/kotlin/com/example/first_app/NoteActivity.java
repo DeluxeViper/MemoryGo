8e7a5f8ec2f672f7193dd9c3f8848521b71a214d
CeruleanSource <abdullah.mohamed@ryerson.ca>
2020-08-29 15:20:02 -0400

    Added popup to native android. Notif config added.

---
+++ android/app/src/main/kotlin/com/example/first_app/NoteActivity.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.first_app.R;
+
+public class NoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+    }
+}
